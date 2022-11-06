086773960a910883f628bc42623bfc4cc839c7c7
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-06 22:32:39 +0100

    Adding cart icon

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -3,23 +3,20 @@ import "./Header.scss";
 
 import Logo from "../Logo/Logo";
 
+// Icon
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
 const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
         <Logo />
 
-        <div className="nav__bag">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-            className="nav__bag"
-          >
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path d="M7 8V6a5 5 0 1 1 10 0v2h3a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h3zm0 2H5v10h14V10h-2v2h-2v-2H9v2H7v-2zm2-2h6V6a3 3 0 0 0-6 0v2z" />
-          </svg>
+        {/* <AiOutlineShoppingCart className="bag" fontSize={30} color="white" /> */}
+
+        <div className="bag__container">
+          <AiOutlineShoppingCart className="bag" fontSize={30} color="white" />
+          <span className="items__number"></span>
         </div>
       </nav>
     </header>
--- src/components/Layout/Logo/Logo.jsx
+++ src/components/Layout/Logo/Logo.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "./Logo.scss";
 
+// Typing effect
+import TypeIt from "typeit-react";
+
 // Routing
 import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
     <Link to="/" className="nav__logo">
-      <span>S</span>hoesit
+      <TypeIt
+        options={{
+          afterComplete: function (instance) {
+            instance.destroy();
+          },
+        }}
+      >
+        <span>S</span>hoesit
+      </TypeIt>
     </Link>
   );
 };
