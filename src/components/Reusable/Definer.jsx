75db0e8aea24c3e3b56846d88ec5f6983c0cd82e
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-06 23:08:46 +0100

    Changes added to definer

--- src/components/Reusable/Definer.jsx
+++ src/components/Reusable/Definer.jsx
@@ -4,11 +4,12 @@ import "./Definer.scss";
 // Link
 import { Link } from "react-router-dom";
 
-const Definer = ({ route, gender, Icon }) => {
+const Definer = ({ route, gender, brand, Icon }) => {
   return (
     <Link to={route} className="definer__card">
       <Icon className="definer__icon" size={45} />
-      <h3>{gender}</h3>
+      {gender && <h3>{gender}</h3>}
+      {brand && <h3>{brand}</h3>}
     </Link>
   );
 };
--- src/pages/Brand.jsx
+++ src/pages/Brand.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Brand.scss";
 
+// Motion
+import { motion } from "framer-motion";
+
+// Definer component
+import Definer from "../components/Reusable/Definer";
+
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <section className="brand">
+      <motion.div className="brand__container container">
+        <div className="brand__data">
+          <h2 className="brand__title">Choose your favorite brand:</h2>
+
+          <div></div>
+        </div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Brand;
