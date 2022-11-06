13c27dc12cc353b15122b94e405313648ed10e0f
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-06 23:15:32 +0100

    Adding brand icons

--- src/pages/Brand.jsx
+++ src/pages/Brand.jsx
@@ -4,8 +4,13 @@ import "./Brand.scss";
 // Motion
 import { motion } from "framer-motion";
 
+// Icons
+import { SiAdidas, SiNike, SiPuma, SiNewbalance, SiReebok } from "react-icons";
+
 // Definer component
 import Definer from "../components/Reusable/Definer";
+
+// Data
 
 const Brand = () => {
   return (
@@ -14,7 +19,17 @@ const Brand = () => {
         <div className="brand__data">
           <h2 className="brand__title">Choose your favorite brand:</h2>
 
-          <div></div>
+          <div>
+            <Definer route="/adidas" Icon={SiAdidas} brand="Adidas" />
+            <Definer route="/nike" Icon={SiNike} brand="Nike" />
+            <Definer route="/reebok" Icon={SiReebok} brand="Reebok" />
+            <Definer route="/puma" Icon={SiPuma} brand="Puma" />
+            <Definer
+              route="/new-balance"
+              Icon={SiNewbalance}
+              brand="New Balance"
+            />
+          </div>
         </div>
       </motion.div>
     </section>
