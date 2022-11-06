79aedd8308082fb508827e075aa21688a955e052
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-06 23:19:09 +0100

    Fixing brand styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import { Routes, Route } from "react-router-dom";
 // Pages
 import Home from "./pages/Home";
 import Gender from "./pages/Gender";
+import Brand from "./pages/Brand";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/gender" element={<Gender />} />
+      <Route path="/gender/:genderType/brand" element={<Brand />} />
     </Routes>
   );
 };
