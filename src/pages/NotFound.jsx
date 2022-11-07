6d8c19f7562281128e564b61f7806ecaf225d994
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-07 12:39:50 +0100

    Not found styles added!

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "./NotFound.scss";
 
+// Link
+import { Link } from "react-router-dom";
+
 // Motion
 import { motion } from "framer-motion";
 
+// Ghost
+import Ghost from "../assets/images/ghost-img.png";
+
 const NotFound = () => {
   return (
-    <section className="notfound">
-      <motion.div
-        whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="notfound__container container"
-      >
+    <motion.section
+      whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+      transition={{ duration: 0.5 }}
+      className="notfound"
+    >
+      <div className="notfound__container container">
         <div className="notfound__data">
           <span className="notfound__subtitle">Error 404</span>
           <h1 className="notfound__title">Hey buddy</h1>
@@ -19,23 +25,23 @@ const NotFound = () => {
             We can't seem to find the page <br /> you are looking for.
           </p>
 
-          <a href="#" class="notfound__button">
+          <Link to="/" class="notfound__button">
             Go Home
-          </a>
+          </Link>
         </div>
 
         <div class="notfound__img">
-          <img src="assets/img/ghost-img.png" alt="" />
+          <img src={Ghost} />
           <div class="notfound__shadow"></div>
         </div>
-      </motion.div>
+      </div>
 
       <footer class="notfound__footer">
         <span>(123) 456-654</span>
         <span>|</span>
         <span>Contact@shoesit.it</span>
       </footer>
-    </section>
+    </motion.section>
   );
 };
 
