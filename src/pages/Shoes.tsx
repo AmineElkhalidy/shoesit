4ffc9d8fe9cb7cbfcdf6e267388ed013223a8e9b
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-12-01 17:23:40 +0100

    Typescript added

--- src/pages/Shoes.tsx
+++ src/pages/Shoes.tsx
@@ -11,7 +11,7 @@ const Shoes = () => {
   return (
     <section className="shoes">
       <motion.div
-        hileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+        whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
         transition={{ duration: 0.5 }}
         className="shoes__container container grid"
       >
